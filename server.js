8dfe374d46bd980b6bb7bb5c4e538d182523ab99
keshavgoyal2807 <keshavgoyal2014@gmail.com>
2020-06-24 22:28:39 +0530

    changed production code

--- server.js
+++ server.js
@@ -135,11 +135,11 @@ app.get('/verify',tokenMiddleware,(req,res)=>{
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
   
     const path = require('path');
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
   
   }
